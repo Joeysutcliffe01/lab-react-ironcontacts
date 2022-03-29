dcbe7989ca84a2d4a04ef60a50fa5167f1c44cb5
Joey Sutcliffe <joeysutcliffe0@gmail.comgit config --global user.email joeysutcliffe0@gmail.com>
2022-03-29 15:14:59 +0200

    It 3 done

--- src/App.js
+++ src/App.js
@@ -1,39 +1,66 @@
 import allContacts from "./contacts.json";
 import "./App.css";
-import {useState} from 'react';
+import { useState } from "react";
 
 function App() {
   const firstFive = allContacts.slice(0.5);
-  const [celebs, setCelebs] = useState(firstFive)
+  const [celebs, setCelebs] = useState(firstFive);
 
-  return <div className="App">
-  <h1>Iron Contacts</h1>
-    <table>
-      <thead>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-        </tr>
-      </thead>
-      <tbody>
-        {celebs.map((elem, index) =>
-        return(
-          <tr key={elem.name + index}> 
-            <td>
-              <img 
-              src={elem.pictureUrl} 
-              alt="celeb pic" 
-              style={{ height: "100px"}}
-              />
-            </td>
+  return (
+    <div className="App">
+      <h1>Iron Contacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+            <th>Actions</th>
           </tr>
-        )
-        
-        )}
-      </tbody>
-    </table>
-  </div>;
+        </thead>
+        <tbody>
+          {celebs.map((elem, index) => {
+            let trophy = "";
+            let emmy = "";
+
+            if (elem.wonOscar === true) {
+              trophy = "🏆";
+            }
+            if (elem.wonEmmy === true) {
+              emmy = "🌟";
+            }
+
+            return (
+              <tr key={elem.name + index}>
+                <td>
+                  <img
+                    src={elem.pictureUrl}
+                    alt="celeb pic"
+                    style={{ height: "100px" }}
+                  />
+                </td>
+                <td>
+                  <h3>{elem.name}</h3>
+                </td>
+                <td>
+                  <h3>{elem.popularity}</h3>
+                </td>
+                <td>
+                  <h3>{trophy}</h3>
+                </td>
+                <td>
+                  <h3>{emmy}</h3>
+                </td>
+                <td>{/* <button> Delete </button> */}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
